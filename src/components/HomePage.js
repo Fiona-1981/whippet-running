32c31bcbdf55dd7ce7348eae4b03eea4b0a13534
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-06-16 18:14:10 +0100

    Added some styling

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { ChakraProvider, Container, Box, Heading } from "@chakra-ui/react";
+import { ChakraProvider, Container, Box, Heading, Text, Image, Center } from "@chakra-ui/react";
+import source from "../whippet.png";
 
 function HomePage() {
 
@@ -7,7 +8,14 @@ function HomePage() {
     <>
       <ChakraProvider>
         <Container>
-          <Heading color='blue' align='center' >Whippet Running</Heading>
+          <Box bg='gray.200' h='100%' >
+          <Heading align='center' >
+            <Text padding='50px' color='blue.700' fontFamily='fantasy' >Whippet Running</Text>
+          </Heading>
+          <Center>
+          <Image padding='40px' src={source}/>
+          </Center>
+          </Box>
         </Container>
       </ChakraProvider>
     </>
