7a29be17a181f865ed7f6c363d11f6b7ccc5262a
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-06-17 08:44:49 +0100

    Added React emojis

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { ChakraProvider, Container, Box, Heading, Text, Image, Center } from "@chakra-ui/react";
+import { ChakraProvider, Container, Box, Heading, Text, Image, Center, Button } from "@chakra-ui/react";
 import source from "../whippet.png";
 
 function HomePage() {
@@ -8,12 +8,16 @@ function HomePage() {
     <>
       <ChakraProvider>
         <Container>
-          <Box bg='gray.200' h='100%' >
+          <Box bg='gray.200' w='100%' h='100%' >
           <Heading align='center' >
             <Text padding='50px' color='blue.700' fontFamily='fantasy' >Whippet Running</Text>
           </Heading>
           <Center>
           <Image padding='40px' src={source}/>
+          </Center>
+          <Text fontSize='2xl' color='blue.700' align='center' paddingBottom='30px' >🏃‍♂️ A Repository of Running 🏃‍♂️</Text>
+          <Center paddingBottom='30px' >
+          <Button variant='solid' border='2px' borderColor='blue.900' colorScheme='blue' _hover={{ bg: '#4da6ff' }} >Have a look</Button>
           </Center>
           </Box>
         </Container>
