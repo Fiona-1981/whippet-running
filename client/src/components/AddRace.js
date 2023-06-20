ea7e4a88c257c467bdb805b8599452de5533db7c
Fiona Wiggins <fiona@fionawiggins.co.uk>
2023-06-20 15:43:55 +0100

    Setting up server

--- client/src/components/AddRace.js
+++ client/src/components/AddRace.js
@@ -1,5 +1,7 @@
 import React from "react";
-import { ChakraProvider, Container, Box, Heading, Text, Button, Stack, FormControl } from "@chakra-ui/react";
+import { ChakraProvider, Container, Box, Heading, Text, Button, Stack, FormControl,
+FormLabel, 
+Input} from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 function AddRace() {
@@ -13,8 +15,17 @@ function AddRace() {
             <Text padding='50px' color='blue.700' fontFamily='fantasy' >Whippet Running</Text>
           </Heading>
           <Text fontSize='2xl' color='blue.700' align='center' paddingBottom='30px' >🏃‍♂️ A Repository of Running 🏃‍♂️</Text>
-          <FormControl>
-            
+          <FormControl padding='30px' >
+          <FormLabel color='blue.700' >Name of race</FormLabel>
+          <Input variant='filled' placeholder='Race name' />
+          <FormLabel paddingTop='10px' color='blue.700'>Date of race</FormLabel>
+          <Input variant='filled' placeholder='Date' />
+          <Button width='120px' variant='solid' border='2px' borderColor='blue.700' colorScheme='blue' _hover={{ bg: '#4da6ff' }} >
+            Save
+            </Button>
+            {/* And here is the monster challenge. On click, send to database,
+            give dialogue "saved!" or something and then when you go back to the 
+            relevant year, the entry appears. */}
           </FormControl>
           <Stack padding='30px' align='left' >
            <Link to={"/menu"}>
